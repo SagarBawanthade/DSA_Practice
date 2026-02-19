90e09de38bb2b01be28deed60258c3b51b846226
SagarBawanthade <sagar.bawanthade2004@gmail.com>
2026-02-19 10:39:13 +0530

    sorting algo

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,26 @@
+class SelectionSort{
+    public static void main(String[] args){
+        int[] arr = {64, 25, 12, 22, 11};
+        selectionSort(arr);
+        System.out.println("Sorted array: " + java.util.Arrays.toString(arr));
+        
+    }
+
+    public static void selectionSort(int[] arr){
+        int n = arr.length;
+
+        for(int i = 0;i<n-1;i++){
+            int min_index = i;
+            for(int j = i+1;j<n;j++){
+                if(arr[j] < arr[min_index]){
+                    min_index = j;
+                }
+            }
+        
+            int temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
+        }
+
+    }
+}
