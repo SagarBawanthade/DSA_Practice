94f401f663da1aa2928348e4bae9f2bab77c0775
SagarBawanthade <sagar.bawanthade2004@gmail.com>
2026-02-21 11:50:51 +0530

    FindMinimumInRotatedSortedArray using BS

---
+++ Searching/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,29 @@
+package Searching;
+
+public class FindMinimumInRotatedSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        int result = findMin(arr);
+        System.out.println(result);
+        
+    }
+    public static int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;  
+            } else {
+                high = mid; 
+            }
+        }
+        
+        return nums[low];   
+        
+        
+    }
+    
+}
