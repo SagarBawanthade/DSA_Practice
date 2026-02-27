1c0b9ce57c566d93aba2d03f96a0163c9c846649
SagarBawanthade <sagar.bawanthade2004@gmail.com>
2026-02-27 20:59:18 +0530

    anitgravty

---
+++ Searching/MinimumNoToMakeBouqets.java
@@ -0,0 +1,15 @@
+package Searching;
+
+class MinimumNoToMakeBouqets {
+    public static void main(String[] args) {
+        int bloomDay[] = {1,10,3,10,2};
+        int m = 3;
+        int k = 1;
+        System.out.println(minDays(bloomDay, m, k));   
+    }
+
+    public static int minDays(int[] bloomDay, int m, int k) {
+        
+    }
+    
+}
