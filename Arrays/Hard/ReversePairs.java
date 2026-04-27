f44cf410e09a6d776d60246a366bccb1b7c3e8af
Sagar Bawanthade <137410094+SagarBawanthade@users.noreply.github.com>
2026-04-27 18:19:34 +0530

    Update ReversePairs.java

--- Arrays/Hard/ReversePairs.java
+++ Arrays/Hard/ReversePairs.java
@@ -7,6 +7,7 @@ public class ReversePairs {
     }
     public static int reversePairs(int[] nums) {
        int count = 0;
+        
 
         for(int i = 0; i < nums.length; i++){
               for(int j = i + 1; j < nums.length; j++){
